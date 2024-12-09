342c90f3f00e171c19a76f0b6f1b3734a6c42ff4
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-09 23:53:00 +0100

    loops

---
+++ javaPractice/LoopPractice/RightSideRightAngle.java
@@ -0,0 +1,17 @@
+public class RightSideRightAngle{
+	public static void main(String[] args){
+		for(int a = 1; a <= 5; a++){
+			for(int b = a; b <= 5; b++){
+				System.out.print(" ");
+			}
+				for(int c = 1; c <= a; c++){
+					System.out.print("*");
+			}	
+				System.out.println();
+			}
+
+
+
+	}
+
+}
